eb92dc80e88f6b2385e8494219691a156c129df9
Zohar Nyego <zoharngo@gmail.com>
2019-12-13 18:33:52 +0200

    split to smaller comopnent and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.style.scss";
+import Dirctory from "../../components/dircetory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Dirctory />
+  </div>
+);
+
+export default HomePage;
